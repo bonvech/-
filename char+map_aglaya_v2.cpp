ab9112c4bcdb76084dfb3603965932b315f75a37
bonvech <bonvech@yandex.ru>
2020-12-13 23:46:10 +0300

    Add files via upload

--- char+map_aglaya_v2.cpp
+++ char+map_aglaya_v2.cpp
@@ -1,13 +1,12 @@
+// Arseniy version 10.12.2020
 #include <stdio.h>
-#include <windows.h>
 #include <TXLib.h>
 
-class Map
+struct Map
 {
-  public:
     HDC map_pic;
     int n, m;
-    int loc[8][10];
+    int loc[9][11];
 
     Map();
     ~Map();
@@ -16,14 +15,13 @@ class Map
     void Set_cell(int x, int y, int type);
 };
 
-class Character
+
+struct Character
 {
-  public:
-    HDC DC;
+    HDC Sprite_list;
     int x, y, a0, b0, a, b, dy, dx, k;
-    int diamond = 0;
+    int point = 0;
     COLORREF color;
-    Map location;
 
     Character();
     ~Character();
@@ -38,55 +36,46 @@ class Character
 
 int main()
 {
-    txCreateWindow(800, 600);
+    txCreateWindow (800, 600);
 
     Map location;
     location.Draw();
 
-    const char* name = txInputBox ("Tell me your name", "System", "Black knight");
+    Character Girl;
+    Girl.Draw();
 
-    Character Boy;
-    Boy.Draw();
-
-    while(!GetAsyncKeyState(VK_ESCAPE))
+    while (!GetAsyncKeyState (VK_ESCAPE))
     {
-        if(GetAsyncKeyState(VK_UP))
-            Boy.Up(location);
+        if (GetAsyncKeyState (0x57))
+            Girl.Up(location);
 
-        if(GetAsyncKeyState(VK_DOWN))
-            Boy.Down(location);
+        if (GetAsyncKeyState (0x53))
+            Girl.Down(location);
 
-        if(GetAsyncKeyState(VK_RIGHT))
-            Boy.Right(location);
+        if (GetAsyncKeyState (0x44))
+            Girl.Right(location);
 
-        if(GetAsyncKeyState(VK_LEFT))
-            Boy.Left(location);
+        if (GetAsyncKeyState (0x41))
+            Girl.Left(location);
 
-        if(location.Get_cell(Boy.x, Boy.y) == 2 && GetAsyncKeyState(VK_SPACE))
+        if(location.Get_cell(Girl.x, Girl.y) == 2 && GetAsyncKeyState(0x45))
         {
-            location.Set_cell(Boy.x, Boy.y, 1);
-            Boy.diamond += 1;
+            location.Set_cell(Girl.x, Girl.y, 1);
+            Girl.point += 1;
         }
-
-
-        if(Boy.x < 80 && Boy.y < 60 && Boy.diamond == 2)
-            break;
-
+        if(Girl.point == 5)
+        {
+            location.Set_cell(600, 560, 1);
+        }
+        if(Girl.point == 5 && location.Get_cell(Girl.x, Girl.y) == 3)
+        {
+             const int finish = txMessageBox("Я хочу пиццу");
+             return 0;
+        }
         location.Draw();
-        Boy.Draw();
+        Girl.Draw();
         txSleep(100);
         txClear();
-    }
-
-    txSetFillColor(TX_BLACK);
-    txClear();
-
-    while(!GetAsyncKeyState(VK_ESCAPE))
-    {
-        txSelectFont ("Times New Roman", 40, 0, FW_BOLD);
-        txDrawText  (200, 100, 600, 300, "CONGRATULATIONS!");
-        txSleep(10000);
-        const char* name = txInputBox ("Go to next level?", "System", "Yes!");
     }
 
     return 0;
@@ -96,105 +85,115 @@ int main()
 
 Character::Character()
 {
-   DC = txLoadImage("sprite_aglaya.bmp");
-   x = 700;
-   y = 380;
-   a0 = 45;
-   b0 = 94;
-   a = 0;
-   b = 0;
-   dx = 10;
-   dy = 10;
-   k = 6;
-   color = TX_WHITE;
+    Sprite_list = txLoadImage ("anime_girl.bmp");
+    x = 30;
+    y = 65;
+    a0 = 42;
+    b0 = 71;
+    a = 0;
+    b = 0;
+    dx = 10;
+    dy = 10;
+    k = 3;
+    color = TX_WHITE;
 }
 
 Character::~Character()
 {
-    txDeleteDC(DC);
+    txDeleteDC (Sprite_list);
 }
 
 void Character::Up(Map &location)
 {
-    if(y <= 40 && diamond < 2)
+     if(y <= 40)
         return;
     if(location.Get_cell(x, y - dy) != 0)
     {
         y -= dy;
-        b = b0 * 2 + 10;
+        b = b0 * 3 + 1;
         a += a0;
-        if(a >= a0 * k)
+
+        if (a >= a0 * k)
             a = a0;
+
     }
 }
 
 void Character::Down(Map &location)
 {
+    if(y >= 570)
+        return;
     if(location.Get_cell(x, y + dy) != 0)
     {
         y += dy;
         b = b0 * 0;
         a += a0;
-        if(a >= a0 * k)
+
+        if (a >= a0 * k)
             a = a0;
     }
 }
 
 void Character::Right(Map &location)
 {
+    if(x >= 680)
+        return;
     if(location.Get_cell(x + dx, y) != 0)
     {
         x += dx;
-        b = b0 * 3 + 12;
+        b = b0 * 2;
         a += a0;
-        if(a >= a0 * k)
+
+        if (a >= a0 * k)
             a = a0;
     }
 }
 
 void Character::Left(Map &location)
 {
-    if(x <= 60 && diamond < 2)
+    if(x <= 20)
         return;
     if(location.Get_cell(x - dx, y) != 0)
     {
         x -= dx;
         b = b0 * 1;
         a += a0;
-        if(a >= a0 * k)
-            a = a0;
-    }
 
+        if (a >= a0 * k)
+            a = a0;
+
+    }
 }
 
 void Character::Draw()
 {
-    txTransparentBlt (txDC(), x - a0/2, y - b0, a0, b0, DC, a, b, color);
-}
-
-
-
-Map::Map()
-{
-    n = 6;
-    m = 10;
-    int l[6][10] = {{1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                    {1, 1, 0, 0, 0, 0, 1, 2, 1, 0},
-                    {0, 1, 0, 0, 1, 1, 1, 0, 1, 0},
-                    {0, 2, 0, 0, 1, 0, 1, 0, 1, 0},
-                    {0, 1, 0, 0, 1, 0, 1, 1, 1, 0},
-                    {0, 1, 1, 1, 1, 0, 0, 0, 1, 0},};
-    map_pic = txLoadImage("map_aglaya.bmp");
-
-    int i, j;
-    for(i = 0; i < n; i += 1)
-        for(j = 0; j < m; j += 1)
-            loc[i][j] = l[i][j];
+    txTransparentBlt (txDC(), x - a0 / 2, y - b0, a0, b0, Sprite_list, a, b, color);
 }
 
 Map::~Map()
 {
      txDeleteDC(map_pic);
+}
+
+Map::Map()
+{
+    n = 9;
+    m = 11;
+    int l[9][11] = {{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                    {1, 1, 0, 0, 0, 0, 1, 2, 1, 0, 0},
+                    {0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0},
+                    {0, 2, 0, 0, 1, 0, 1, 0, 1, 0, 0},
+                    {0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0},
+                    {0, 1, 2, 1, 1, 0, 1, 0, 1, 0, 0},
+                    {0, 1, 0, 0, 1, 0, 2, 0, 1, 0, 0},
+                    {0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0},
+                    {0, 1, 1, 1, 2, 0, 0, 0, 1, 0, 3}};
+    map_pic = txLoadImage("map_pic.bmp");
+
+    int i, j;
+    for(i = 0; i < n; i += 1)
+        for(j = 0; j < m; j += 1)
+            loc[i][j] = l[i][j];
 }
 
 void Map::Draw()
@@ -204,14 +203,18 @@ void Map::Draw()
     {
         for(j = 0; j < m; j += 1)
         {
-            txTransparentBlt(txDC(), j*80, i*60, 100, 85, map_pic, 214, 82, TX_WHITE);
+            txTransparentBlt(txDC(), j*64, i*64, 64, 64, map_pic, 0, 0, TX_WHITE);
             if(loc[i][j] == 1)
             {
-                txTransparentBlt(txDC(), j*80, i*60, 100, 85, map_pic, 85, 82, TX_WHITE);
+                txTransparentBlt(txDC(), j*64, i*64, 64, 64, map_pic, 0, 64, TX_WHITE);
             }
             if(loc[i][j] == 2)
             {
-                txTransparentBlt(txDC(), j*80, i*60, 100, 85, map_pic, 330, 108, TX_WHITE);
+                txTransparentBlt(txDC(), j*64, i*64, 64, 64, map_pic, 64, 64, TX_WHITE);
+            }
+            if(loc[i][j] == 3)
+            {
+                txTransparentBlt(txDC(), j*64, i*64, 64, 64, map_pic, 125, 64, TX_WHITE);
             }
         }
     }
@@ -220,12 +223,14 @@ void Map::Draw()
 
 int  Map::Get_cell(int x, int y)
 {
-    int i = x / 80;
-    int j = y / 60;
+    int i = x / 64;
+    int j = y / 64;
     return loc[j][i];
 }
 
 void Map::Set_cell(int x, int y, int type)
 {
-    loc[y / 60][x / 80] = type;
+    loc[y / 64][x / 64] = type;
 }
+
+
